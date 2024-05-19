b855ee5793c74c0a2a223b889c5542a6f79a4f4f
Makarand Hinge <mmh2212002@gmail.com>
2024-05-19 14:33:50 +0530

    Java Program to Find the Number of Integers Divisible by 5

---
+++ program/src/com/example/program/DivisibleByFive.java
@@ -0,0 +1,22 @@
+//Java Program to Find the Number of Integers Divisible by 5
+
+package com.example.program;
+
+import java.util.Scanner;
+
+public class DivisibleByFive {
+
+	public static void main(String[] args) {
+		 Scanner sc = new Scanner(System.in);
+		 System.out.println("Check whether the number is divisible by 5 or not");
+		 System.out.print("Enter the number --> ");
+		 int x = sc.nextInt();
+		 if(x % 5 == 0) {
+			 System.out.println(x + " is Divisible by 5 ");
+		 }else {
+			 System.out.println(x + " is not Divisible by 5 ");
+		 }
+
+	}
+
+}
