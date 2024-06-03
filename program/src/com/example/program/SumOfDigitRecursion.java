1e3f856ed90080e41891722d16aae7cd0ca562ad
Makarand Hinge <mmh2212002@gmail.com>
2024-06-03 22:24:41 +0530

    Updated with proper code Comment Java Program to Find Sum of Digits of a Number using Recursion

--- program/src/com/example/program/SumOfDigitRecursion.java
+++ program/src/com/example/program/SumOfDigitRecursion.java
@@ -1,6 +1,37 @@
 //Java Program to Find Sum of Digits of a Number using Recursion
-
 package com.example.program;
+
+/*
+		import java.util.Scanner;
+public class Digit_Sum 
+{
+    int sum = 0;
+    public static void main(String[] args) 
+    {
+        int n;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter the number:");
+        n = s.nextInt();
+        Digit_Sum obj = new Digit_Sum();
+        int a = obj.add(n);
+        System.out.println("Sum:"+a);
+    }
+    int add(int n)
+    {
+        sum = n % 10;
+        if(n == 0)
+        {
+            return 0;
+        }
+        else
+        {
+             return sum + add(n / 10);
+        }
+ 
+    }
+}
+	*/
+
 
 import java.util.Scanner;
 
