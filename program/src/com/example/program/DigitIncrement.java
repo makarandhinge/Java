093793745688507381de4a77cfd1081797942c8c
Makarand Hinge <mmh2212002@gmail.com>
2024-06-11 19:06:55 +0530

    Java Program to Increment by 1 to all the Digits of a Given Integer

---
+++ program/src/com/example/program/DigitIncrement.java
@@ -0,0 +1,34 @@
+//Java Program to Increment by 1 to all the Digits of a Given Integer
+
+package com.example.program;
+
+import java.util.Scanner;
+public class DigitIncrement {
+
+	public static void main(String[] args) {
+		
+		int n, m = 0, a;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter any number:");
+        n = s.nextInt();
+        while(n > 0)
+        {
+            a = n % 10;
+            a++;
+            m = m * 10 + a;
+            n = n / 10;
+        }
+        n = m;
+        m = 0;
+        while(n > 0)
+        {
+            a = n % 10;
+            m = m * 10 + a;
+            n = n / 10;
+        }
+        System.out.println("Result:"+m);
+		
+
+	}
+
+}
