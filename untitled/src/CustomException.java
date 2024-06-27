60300b0aa28bb9267501f52c11acc112a55be99c
Makarand Hinge <mmh2212002@gmail.com>
2024-06-27 13:58:27 +0530

    EXception Handling

---
+++ untitled/src/CustomException.java
@@ -0,0 +1,5 @@
+public class CustomException extends RuntimeException{
+    public CustomException(String msg){
+        super(msg);
+    }
+}
---
+++ untitled/src/HandlingException.java
@@ -0,0 +1,19 @@
+public class HandlingException {
+    public static void main(String[] args) {
+        m1();
+
+    }
+
+    private static void m1(){
+        m2();
+    }
+
+    private static void m2() {
+       try{
+           System.out.println(10/0);
+       }catch(ArithmeticException e){
+           System.out.println(10/2
+           );
+       }
+    }
+}
