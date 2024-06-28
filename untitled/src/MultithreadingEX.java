846d247068281aa262fc02351bfdf31774502192
Makarand Hinge <mmh2212002@gmail.com>
2024-06-28 14:06:56 +0530

    Technical Lecture

---
+++ untitled/src/MultithreadingEX.java
@@ -0,0 +1,12 @@
+public class MultithreadingEX {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        MyThread t = new MyThread();
+        t.start();
+       // t.join();
+        for(int i = 1;i<10;i++){
+            System.out.println("I am Main Thread");
+        }
+    }
+}
---
+++ untitled/src/MyRunnable.java
@@ -0,0 +1,8 @@
+public class MyRunnable implements Runnable{
+    public void run(){
+        for (int i = 1; i < 10; i++) {
+            System.out.println("I am Child Thread");
+
+    }
+}
+    }
