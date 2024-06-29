15f41f5f1d5278f973b83c382cd55d78c38cbeca
Makarand Hinge <mmh2212002@gmail.com>
2024-06-29 14:14:22 +0530

    Technical Lec Multi threading

--- untitled/src/MyThread.java
+++ untitled/src/MyThread.java
@@ -2,8 +2,8 @@ public class MyThread extends Thread {
 
     @Override
     public void run() {
-        for (int i = 1; i < 10; i++) {
-            System.out.println("I am Child Thread");
+        for (int i = 1; i < 20; i++) {
+            System.out.println("I am Child Thread " + i );
         }
     }
 }
