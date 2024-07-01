1fd87f289a973967819b8900749e9991060d91be
Makarand Hinge <mmh2212002@gmail.com>
2024-07-02 00:21:51 +0530

    DSA 450

---
+++ untitled/src/DS450/ReverseArray.java
@@ -0,0 +1,19 @@
+package DS450;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        int[] no = {2, -9, 0, 5, 12, -25, 22, 9, 8,12};
+        int temp;
+        int i=0,j=no.length - 1;
+        while(i < no.length/2){
+            temp = no[i];
+            no[i] = no[j];
+            no[j] = temp;
+            i++;
+            j--;
+        }
+        for(int m : no){
+            System.out.print(m + " ");
+        }
+    }
+}
