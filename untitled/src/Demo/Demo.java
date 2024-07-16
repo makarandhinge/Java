e9bbad07bc20b906448cfcfe647192a65f31b60b
Makarand Hinge <mmh2212002@gmail.com>
2024-07-16 19:54:44 +0530

    Practice program pattern

--- untitled/src/Demo/Demo.java
+++ untitled/src/Demo/Demo.java
@@ -1,32 +1,15 @@
 package Demo;
 
-import java.util.ArrayList;
-import java.util.List;
+import javax.sound.midi.Soundbank;
+import java.util.Scanner;
 
-public class Demo {
-
-
-
+public class Demo{
     public static void main(String[] args) {
 
-        Number n = new Number();
-
-        List l = new ArrayList();
-        l.add(n.getid());
-        System.out.println(l);
-
-    }
-}
-
-class Number{
-    private Long id = 1l;
-    private float unit;
+        int x = -10;
+        System.out.println(x >>> 1);
+        System.out.println("sum"+10+20);
 
 
-    public Long getid(){
-        return id;
-    }
-    public float getunit(){
-        return unit;
     }
 }
