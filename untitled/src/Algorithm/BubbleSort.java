747f3ea60dd9d6ef1c757e700467a9d5ad84d93a
Makarand Hinge <mmh2212002@gmail.com>
2024-07-28 09:45:31 +0530

    Modified Bubble sort and make it find Nth largest number using bubble sort algorithm

--- untitled/src/Algorithm/BubbleSort.java
+++ untitled/src/Algorithm/BubbleSort.java
@@ -2,22 +2,23 @@ package Algorithm;
 
 public class BubbleSort {
     public static void main(String[] args) {
-
+        int Nth = 5;
         int arr[] = {8,2,10,5,3,13,9};
 //        int arr[] = {2,3,5,8,9,10,13};
         Solution s = new Solution();
-        int sort[] = s.sorting(arr);
-        for(int x : sort){
-            System.out.print(x + " ");
-        }
+//        int sort[] = s.sorting(arr,Nth);
+//        for(int x : sort){
+//            System.out.print(x + " ");
+//        }
+        System.out.println(s.sorting(arr,Nth));
     }
 }
 
 class Solution {
-    int[] sorting(int arr[]){
+    int sorting(int arr[],int Nth){
         int flag;
         int n = arr.length;
-        for(int i=0;i<n-1;i++){
+        for(int i=0;i<n-(n-Nth);i++){
             flag = 0;
             for(int j=0;j<n-1-i;j++){
                 if(arr[j] > arr[j+1]){
@@ -29,6 +30,7 @@ class Solution {
             }
             if(flag == 0)break;
         }
-        return arr;
+
+        return  arr[n-Nth];
     }
 }
